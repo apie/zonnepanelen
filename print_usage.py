12703ece85f10f4fdeb220e26d64fa66907d80ea
Denick <1049485+apie@users.noreply.github.com>
2019-07-20 14:46:52 +0200

    Only print recent data and do not print current usage

--- print_usage.py
+++ print_usage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # By Apie, jun 2019
+from datetime import date, timedelta
 from fetch_usage import fetch_current_usage
 from operator import itemgetter
 from zonnepanelen_dbs import db_m, db_c, db_d
@@ -8,28 +9,32 @@ DATE_COL_LEN = 20
 USAGE_COL_LEN = 10
 USAGE_COL_H_LEN = USAGE_COL_LEN + 5
 
+today = date.today()
+
 def list_month_usage():
-    for day in sorted(db_m, key=itemgetter('date')):
-        netto = day['usage']['accumulatedConsumption'] - day['usage']['accumulatedProduction']
+    one_year_ago = today - timedelta(days=365)
+    for day in sorted(db_m("date") > one_year_ago, key=itemgetter('date')):
+        c, p = day['usage']['accumulatedConsumption'], day['usage']['accumulatedProduction']
         print('{:<{dfill}} {:>{ufill}.0f} kWh'.format(
-            day['date'].strftime('%Y-%m'), netto,
+            day['date'].strftime('%Y-%m'), c-p,
             dfill=DATE_COL_LEN, ufill=USAGE_COL_LEN,
         ))
 
 def list_day_usage():
-    for day in sorted(db_d, key=itemgetter('date')):
-        netto = day['usage']['accumulatedConsumption'] - day['usage']['accumulatedProduction']
+    five_days_ago = today - timedelta(days=5)
+    for day in sorted(db_d("date") > five_days_ago, key=itemgetter('date')):
+        c, p = day['usage']['accumulatedConsumption'], day['usage']['accumulatedProduction']
         print('{:<{dfill}} {:>{ufill}.0f} kWh'.format(
-            day['date'].strftime('%Y-%m-%d'), netto,
+            day['date'].strftime('%Y-%m-%d'), c-p,
             dfill=DATE_COL_LEN, ufill=USAGE_COL_LEN,
         ))
 
 def list_current_usage():
     # Return latest record of current usage
     for day in sorted(db_c, key=itemgetter('datetime'), reverse=True):
-        netto = day['usage']['consumption'] - day['usage']['production']
+        c, p = day['usage']['consumption'], day['usage']['production']
         return '{:<{dfill}} {:>{ufill}.3f} kW'.format(
-            day['datetime'].strftime('%Y-%m-%d %H:%M'), netto,
+            day['datetime'].strftime('%Y-%m-%d %H:%M'), c-p,
             dfill=DATE_COL_LEN, ufill=USAGE_COL_LEN,
         )
 
@@ -42,5 +47,4 @@ if __name__ == '__main__':
     print('='*(DATE_COL_LEN + USAGE_COL_H_LEN))
     list_month_usage()
     list_day_usage()
-    print(list_current_usage())
 
